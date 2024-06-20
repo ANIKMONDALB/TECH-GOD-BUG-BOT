260c55efff713c53b3918952b14e6ff6f45913bf
ANIKMONDALB <158838149+ANIKMONDALB@users.noreply.github.com>
2024-06-20 16:33:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "918617340627"
+global.ownername = "꧁༒☬☠An!k☠︎☬༒꧂"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '918617340627'  //creator number
+global.ownername = '꧁༒☬☠An!k☠︎☬༒꧂' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
