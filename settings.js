11ebb0c81d67772ead5ea83512c5b51be5128b58
ANIKMONDALB <158838149+ANIKMONDALB@users.noreply.github.com>
2024-08-30 11:13:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "917718606171"
+global.ownername = "DARK_X_ANIK"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '917718606171'  //creator number
+global.ownername = 'DARK_X_ANIK' //owner name
+global.botname = '☠️𝗗𝗔𝗥𝗞_X-BUG-BOT👾😈⚠️' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'DARK_X_ANIK\n\nContact: +917718606171'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -39,7 +39,7 @@ global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
 
